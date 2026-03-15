6f5132c7469f80b2d09bc46ac13c6eb58bc63395
Landysh Khusaenova <landysh.kh@gmail.com>
2026-03-15 18:39:43 +0100

    Product class created

---
+++ products.py
@@ -0,0 +1,50 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self) -> int:
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.deactivate()
+        else:
+            self.activate()
+
+    def is_active(self) -> bool:
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+    def buy(self, quantity) -> float:
+        self.quantity -= quantity
+        if self.quantity == 0:
+            self.deactivate()
+
+        total_price = self.price * quantity
+        return total_price
+
+
+bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+print(bose.buy(50))
+print(mac.buy(100))
+print(mac.is_active())
+
+bose.show()
+mac.show()
+
+bose.set_quantity(1000)
+bose.show()
